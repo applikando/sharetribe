fc948a8ff6a4c330111d21dc316317bc4c17e371
krazedkrish <krazedkrish@gmail.com>
2015-09-29 15:11:39 +0545

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,46 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# lock '3.3.5'
+set :application, 'sharetribe'
+set :repo_url, 'https://github.com/applikando/sharetribe'
+
+set :chruby_ruby, 'ruby-2.1.2'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+set :user, 'deploy'
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/#{fetch(:user)}/nginx/#{fetch(:application)}"
+
+set :ssh_options, {
+  user: fetch(:user),
+  forward_agent: true
+}
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 3
+
+
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
